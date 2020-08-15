20d4ba7ae2c2668240330f2547d5bbfd9aaaad2c
qdlishi <55542482+qdlishi@users.noreply.github.com>
2020-08-16 01:32:36 +0800

    碰撞

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Collider2D groundCollider;
+    public COLOR color;
+    private void Awake()
     {
-        
+        groundCollider = transform.GetComponent<Collider2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "circle")
+        {
+            Circle circle = collision.transform.GetComponent<Circle>();
+            if (color != circle.color)
+            {
+                groundCollider.isTrigger = false;
+            }
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("碰撞开始");
+        groundCollider.isTrigger = true;
     }
 }
