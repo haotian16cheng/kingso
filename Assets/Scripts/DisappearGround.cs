ab521303d115e884945377c19b0be28627e11741
qdlishi <55542482+qdlishi@users.noreply.github.com>
2020-08-16 11:51:35 +0800

    碰撞消失

---
+++ Assets/Scripts/DisappearGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearGround : Ground
+{
+    public float time;
+    public override void OnBounce(Circle circle)
+    {
+        base.OnBounce(circle);
+        StartCoroutine(Countdown());
+    }
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(0.1f);
+        gameObject.SetActive(false);
+        if(time>=0)
+        {
+            yield return new WaitForSeconds(time);
+            gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/ViscosityGround.cs
+++ Assets/Scripts/ViscosityGround.cs
@@ -7,7 +7,8 @@ public class ViscosityGround : Ground
     public override void OnPass(Circle circle)
     {
         base.OnPass(circle);
-        float angle = Vector2.Angle(new Vector2(Mathf.Cos((float)transform.eulerAngles.z * Mathf.PI / 180), -Mathf.Sin((float)transform.eulerAngles.z * Mathf.PI / 180)), circle.rigidbody2D.velocity);
+        float angle = Vector2.SignedAngle(new Vector2(Mathf.Cos((float)transform.eulerAngles.z * Mathf.PI / 180), -Mathf.Sin((float)transform.eulerAngles.z * Mathf.PI / 180)), circle.rigidbody2D.velocity);
+        Debug.Log(angle);
         if (angle > 0 && angle < 180)
         {
             circle.rigidbody2D.velocity = new Vector2(Mathf.Sin((float)transform.eulerAngles.z * Mathf.PI / 180), -Mathf.Cos((float)transform.eulerAngles.z * Mathf.PI / 180)) * circle.speed;
