6a3ce0a244b3a180832af1a9ae41d88fe76896e1
qdlishi <55542482+qdlishi@users.noreply.github.com>
2020-08-16 14:04:08 +0800

    消失

--- Assets/Scripts/DisappearGround.cs
+++ Assets/Scripts/DisappearGround.cs
@@ -8,16 +8,16 @@ public class DisappearGround : Ground
     public override void OnBounce(Circle circle)
     {
         base.OnBounce(circle);
-        StartCoroutine(Countdown());
+        gameObject.SetActive(false);
+        if (time >= 0)
+        {
+            StartCoroutine(Countdown());
+        }  
     }
     IEnumerator Countdown()
     {
-        yield return new WaitForSeconds(0.1f);
-        gameObject.SetActive(false);
-        if(time>=0)
-        {
-            yield return new WaitForSeconds(time);
-            gameObject.SetActive(true);
-        }
+
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(true);
     }
 }
