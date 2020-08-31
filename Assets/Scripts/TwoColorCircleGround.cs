f5e344e4dec1f2d66dc509233e31e3bccd92e81d
qdlishi <55542482+qdlishi@users.noreply.github.com>
2020-08-31 23:54:56 +0800

    双色球型板

---
+++ Assets/Scripts/TwoColorCircleGround.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoColorCircleGround : TwoColorGround
+{
+    public override void OnBounce(Circle circle)
+    {
+        Vector2 normal = transform.position - circle.transform.position;
+        circle.rigidbody2D.velocity = Vector2.Reflect(circle.rigidbody2D.velocity, normal.normalized);
+    }
+}
